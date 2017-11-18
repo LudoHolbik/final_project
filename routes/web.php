d1e41530510d7f2d6cfc278d9b6dec90cc32d8d1
LuisVuitton <holbikl@gmail.com>
2017-11-18 16:09:20 +0100

    change in routes

--- routes/web.php
+++ routes/web.php
@@ -57,18 +57,10 @@ Route::get('/places/7', 'PlacesController@Cinemas');
 //URL for the whole map
 Route::get('/places/map', 'PlacesController@map');
 Route::post('/places/map/filter', 'PlacesController@mapFilter');
-
-<<<<<<< HEAD
-
-
 Route::get('/places/{id?}', 'PlacesController@index')->name('places');
 Route::get('/like','likeController@like');
 Route::get('/dislike','likeController@dislike');
-=======
 Route::get('/places/detail/{id}', 'PlacesController@placeDetail');
-
 //Route::get('/places/detail/{id}', 'PlacesController@mapPlace');
-
 Route::post('/places/detail/{id}', 'PlacesController@createReview');
 //Route::get('/places/detail/{id}' , 'PlacesController@getReview');
->>>>>>> b3b00ca18e38895946a3d92467a4d369254269c8
