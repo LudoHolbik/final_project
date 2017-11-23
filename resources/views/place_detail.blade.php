2d030d4bd7b1310b444ff83eeafc756ef5dd4b95
Zeynep Artok <artokzeynep@gmail.com>
2017-11-23 13:06:49 +0100

    zey design

---
+++ resources/views/place_detail.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{$place['name']}}</title>
+    <script src="https://use.fontawesome.com/86896db091.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="detail.css">
+</head>
+
+<body>
+    
+
+    <div class="container">
+        <div class="row">
+            <div class="col-9 wrap">
+                <div class="place-info">
+                    <span class="place-name">{$place['name']}}</span>
+                    <div class="details">
+                        <span class="type">{$place['type']}}</span>
+                        <span class="address">{$place['address']}}/span>
+                        <span class="description">{$place['description']}}</span>
+                        <span class="phone">{$place['telephone']}}</span>
+                        <span class="wifi">{$place['wifi']}}</span>
+                        <span class="hours">{$place['opening_hours']}}</span>
+                        <span class="map-link"> <a href="">Show in map</a> </span>
+
+                    </div>
+                </div>
+                <div class="place-pic">
+                    <img src="img/met.jpg" alt="">
+                </div>
+                
+                <div class="container ">
+                    <div class= "row feed">
+                <div class="col-8 ">
+                    <div class="add-post">
+                        <textarea name="" placeholder=" Write Your Review" id="review" cols="60" rows="5"></textarea>
+                        <div class="button-box">
+                            <button class="btn"><i class="fa fa-plus" aria-hidden="true"></i>Add Picture</button>
+                            
+                            <button class="btn"><i class="fa fa-paper-plane" aria-hidden="true"></i>Post</button>
+                        </div>
+                        
+                    </div>
+                    <div class="post">
+                           <div class="post-id">
+                                <img src="img/1.jpg" alt="">
+                                <span class="post-name">Zeynep Artok</span>
+                        </div> 
+
+                            <div class="post-content">
+                                <p>
+                                    One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.
+                                </p>
+                                <button class="btn"><i class="fa fa-thumbs-up" aria-hidden="true"></i>Like</button>
+                                
+                                <button class="btn"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i>Dislike</button>
+
+                                 </div>
+
+                    </div>
+
+                </div>
+            </div>
+            </div>
+           
+
+
+            </div>
+        </div>
+    </div>
+
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- resources/views/places.blade.php
+++ resources/views/places.blade.php
@@ -6,66 +6,60 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="_token" content="{{ csrf_token() }}">
     <title>Places</title>
-
- <link rel="stylesheet" href="/css/places.css">
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat" rel="stylesheet">
-
+    <link rel="stylesheet" href="/css/places.css">
 
 </head>
 <body>
 
 
-
-
-        <div class="container">
-            <div class="row">
-                    <img class="logo "src="/img/logo.png" alt="">
-                <div class="col-8 header">
-
-                </div>
-            </div>
-        </div>
+@include ('header')
 
 <!--button for changing map and cards -->
         <div class="wrap">
-          		<a  href="/places/map" class="button">Map</a>
-                    <a  href="/places" class="button2">Cards</a>
+          		<a href="/places/map" class="button">Map</a>
+                    <a href="/places" class="button2">Cards</a>
         	</div>
 
 
 
         <section class="tabs">
-
         <form id="tabs" action="" method="post">
 
-
-       
-
-
-            <a href="{{ route('places') }}">All places</a>
-
-          
-            <a href="{{ route('places', ['id' => 1]) }}">Best Views</a>
-
-            <a href="{{ route('places', ['id' => 2]) }}">Outdoors &amp; Summer hangouts</a>
-
-            <a href="{{ route('places', ['id' => 3]) }}">Hotels &amp; Hostels</a>
-
-            <a href="{{ route('places', ['id' => 4]) }}">Art galeries</a>
-
-            <a href="{{ route('places', ['id' => 5]) }}">Cultural centres</a>
-
-            <a href="{{ route('places', ['id' => 6]) }}">Cinemas</a>
-
-            <a href="{{ route('places', ['id' => 7]) }}">??</a>
             
+            <input id="tab0" type="radio" name="tabs">
+            <label for="tab0"><a href="{{ route('places') }}">All places</a></label>
+
+            <input id="tab1" type="radio" name="tabs">
+            <label for="tab1"><a href="{{ route('places', ['id' => 1]) }}">Best Views</a></label>
+
+            <input id="tab2" type="radio" name="tabs">
+            <label for="tab2"><a href="{{ route('places', ['id' => 2]) }}">Outdoors &amp; Summer hangouts</a></label>
+
+            <input id="tab3" type="radio" name="tabs">
+            <label for="tab3"><a href="{{ route('places', ['id' => 3]) }}">Hotels &amp; Hostels</a></label>
+
+            <input id="tab4" type="radio" name="tabs">
+            <label for="tab4"><a href="{{ route('places', ['id' => 4]) }}">Art galeries</a></label>
+
+            <input id="tab5" type="radio" name="tabs">
+            <label for="tab5"><a href="{{ route('places', ['id' => 5]) }}">Cultural centres</a></label>
+
+            <input id="tab6" type="radio" name="tabs">
+            <label for="tab6"><a href="{{ route('places', ['id' => 6]) }}">Cinemas</a></label>
+
+            <input id="tab7" type="radio" name="tabs">
+            <label for="tab7"><a href="{{ route('places', ['id' => 7]) }}">??</a></label>
+
+
         </form>
         </section>
 
 <!--
     position:absolute
-
+        
 
     display: flex;
     flex-wrap: wrap;
@@ -98,16 +92,12 @@
                                 <span><a href="">Show more<</a></span> 
                                                               
                                 
-                                <button onclick="likeHandler({{$place['id']}},this);" class="like" data-id="{{$place['id']}}">LIKE</button>  
-                                <button onclick="dislikeHandler({{$place['id']}},this);" class="dislike" data-id="{{$place['id']}}">DISLIKE</button> 
+                                <button class="like" data-id="{{$place['id']}}">LIKE</button>  
+                                <button class="dislike" data-id="{{$place['id']}}">DISLIKE</button> 
 
-
-                                <button class="like" data-id="{{$place['id']}}">LIKE</button>
-                                <button class="dislike" data-id="{{$place['id']}}">DISLIKE</button>
-
-                                <div id="likes"></div>
-
-
+                                <div id="likes"></div>                                                                   
+                                
+                              
 
 
                                 <span><a href="/places/detail/{{ $place -> id }}">Show detail</a></span>
@@ -130,57 +120,71 @@
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
 
-<script src="/js/places.js"></script>
+<script src="/js/places.js"></script> 
 
 <script>
     
-/*-----------
-LIKE FUNCTION
-------------*/
+    /*-----------
+    LIKE FUNCTION
+    ------------*/
 
-function likeHandler(id, button){
-    var likeButton = $(button);/*'.like[data-id='+id+']'); */
-    if (likeButton.html() === 'LIKE'){
-        likeButton.html("UNDO");
-    }else if(likeButton.html() === 'UNDO'){
-        likeButton.html('LIKE');
-    }
+$(function(){
+    $('.like').click(function()
+        {                                                   
+            // menit text na liked  
+            if ($(this).text() === 'LIKE'){
+                $(this).text('UNDO');
+            }else{
+                ($(this).text() === 'UNDO');
+                $(this).text('LIKE');
+            }
             
-    $.ajax({
-        "url" : "/like",
-        "type" : "get",
-        "data" : {
-            "id": id
-        }
-    }).done(function(data) {
-        alert("success");
-    });
-}
-
-/*------------
-DISLIKE FUNCTION
--------------*/        
-
-function dislikeHandler(id, button){
-    var dislikeButton = $(button); // ('.dislike[data-id='+id+']');
-        if (dislikeButton.html() == 'DISLIKE'){
-        dislikeButton.html("UNDO");
-    }else if(dislikeButton.html() == 'UNDO'){
-        dislikeButton.html("DISLIKE");
-    }
+            $.ajax({
+            "url" : "/like",
+            "type" : "get",
+            "data" : {
+                "id": $('.like').data("id")
+            }
+        })
+        .done(function(data) {
+            alert( "success" );
+        })
+    });   
+});
+        // var X = TRUE
+        //prebehne AJAX posli na controller
+    
+    /*------------
+    DISLIKE FUNCTION
+    -------------*/        
+  
+$(function(){
+    $('.dislike').click(function()
+        {                                                   
+            // menit text na liked  
+            if ($(this).text() === 'DISLIKE'){
+                $(this).text('UNDO');
+            }else{
+                ($(this).text() === 'UNDO');
+                $(this).text('DISLIKE');
+            }
             
-    $.ajax({
-        "url" : "/dislike",
-        "type" : "get",
-        "data" : {
-            "id": id        
-        }
-    }).done(function(data) {
-        alert("success");
+            $.ajax({
+            "url" : "/dislike",
+            "type" : "get",
+            "data" : {
+                "id": $('.dislike').data("id")
+            }
+        })
+        .done(function(data) {
+            alert( "success" );
+        })
     });
-}
 
-                                      
+});
+        // var X = FALSE
+        // prebehne AJAX posli na controller
+                                                    
                                     
 </script>
 
